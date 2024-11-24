5c8598eafff88a7ff726a73d90ed60362d6206bb
ebentarigan <ebenezer0852@gmail.com>
2024-11-24 22:29:47 +0700

    memperbarui fitur web

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Header</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="layout/style.css">
 </head>
 <body>
 <?php
     include "layout/header.html";
 
 ?>
+<main>
+  <p>Halo,selamat datang di website kami</p>
+</main>
 
 <!-- footer -->
 <?php
--- register.php
+++ register.php
@@ -1,23 +1,47 @@
+<?php
+include "db_config.php";
+session_start();
+$register_message='';
+$username = '';
+$password = '';
+$role = '';
+
+if (isset($_POST['register'])) {
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+    $role = isset($_POST['role']) ? $_POST['role'] : '';
+
+    // Query untuk menyimpan data ke database
+    $sql = "INSERT INTO users(username, password,role) VALUES ('$username', '$password','user');";
+    if (!$db->query($sql)) {
+        $register_message ="daftar akun anda gagal,silahkan coba lagi" . $db->error;
+    } else{
+        $register_message ="daftar akun anda berhasil,silahkan login";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Header</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Register Akun</title>
+    <link rel="stylesheet" href="layout/style.css">
 </head>
 <body>
 <?php
-    include "layout/header.html"
+    include "layout/header.html";
 ?>
 <h3>Register Akun</h3>
-<form>
-    <input type="text" placeholder="username" name="username"/>
-    <input type="password" placeholder="password" name="pasword"/>
-    <button type="submit">masuk sekarang</button>
+<i><?=$register_message?></i>
+<form action="register.php" method="POST">
+    <input type="text" placeholder="username" name="username" required />
+    <input type="password" placeholder="password" name="password" required />
+    <button type="submit" name="register">Daftar Sekarang</button>
 </form>
-<?php
-    include "layout/footer.html"
-?>
+
 </body>
 </html>
+
+
